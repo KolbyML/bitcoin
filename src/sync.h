79a4b455c10a5946eaf30a1f6c79a7e70dd32bf4
Fuzzbawls <fuzzbawls@gmail.com>
2018-11-15 04:36:19 -0800

    Finalize cleanup of sync.cpp/h
    - include memory system header for std pointers
    - remove unused MutexName
    - declare single-argument constructors as explicit

--- src/sync.h
+++ src/sync.h
@@ -205,7 +205,7 @@ private:
     int value;
 
 public:
-    CSemaphore(int init) : value(init) {}
+    explicit CSemaphore(int init) : value(init) {}
 
     void wait()
     {
@@ -269,13 +269,12 @@ public:
         grant.Release();
         grant.sem = sem;
         grant.fHaveGrant = fHaveGrant;
-        sem = NULL;
         fHaveGrant = false;
     }
 
     CSemaphoreGrant() : sem(nullptr), fHaveGrant(false) {}
 
-    CSemaphoreGrant(CSemaphore& sema, bool fTry = false) : sem(&sema), fHaveGrant(false)
+    explicit CSemaphoreGrant(CSemaphore& sema, bool fTry = false) : sem(&sema), fHaveGrant(false)
     {
         if (fTry)
             TryAcquire();
@@ -288,7 +287,7 @@ public:
         Release();
     }
 
-    operator bool()
+    operator bool() const
     {
         return fHaveGrant;
     }
