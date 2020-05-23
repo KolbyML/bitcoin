4a9c7e71f5201079451f498a0596fb18443677a3
Kolby Moroz <mrmetech@gmail.com>
2020-05-23 16:46:53 -0600

    Update rpcwallet.cpp

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4402,8 +4402,6 @@ static void ParseRecords(
     size_t  nWatchOnly  = 0;
     CAmount totalAmount = 0;
 
-    auto locked_chain = pwallet->chain().lock();
-
     int confirmations = wtx.GetDepthInMainChain(locked_chain);
     entry.__pushKV("confirmations", confirmations);
     if (confirmations > 0) {
