42d5e9dab9549d95630c0cb2f7a38b1d18f57a48
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2020-09-05 13:49:09 -0600

    Update rpcserver.h

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -338,10 +338,9 @@ extern UniValue getstakingstatus(const UniValue& params, bool fHelp);
 
 extern UniValue mnspork(const UniValue& params, bool fHelp);
 extern UniValue masternode(const UniValue& params, bool fHelp);
-extern UniValue listmasternode(const UniValue& params, bool fHelp);
+extern UniValue listmasternodes(const UniValue& params, bool fHelp);
 extern UniValue getmasternodestatus(const UniValue& params, bool fHelp);
 extern UniValue masternodeconnect(const UniValue& params, bool fHelp);
-extern UniValue masternodecurrent(const UniValue& params, bool fHelp);
 extern UniValue masternodedebug(const UniValue& params, bool fHelp);
 extern UniValue createmasternodekey(const UniValue& params, bool fHelp);
 extern UniValue getmasternodeoutputs(const UniValue& params, bool fHelp);
