2062f0fb8fff8d64347174100ddffc3d700e392c
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2020-09-13 12:13:34 -0600

    more removing stuff

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -16,7 +16,7 @@
 #include "optionsmodel.h"
 #include "overviewpage.h"
 #include "receivecoinsdialog.h"
-#include "privacydialog.h"
+
 #include "sendcoinsdialog.h"
 #include "signverifymessagedialog.h"
 #include "transactiontablemodel.h"
