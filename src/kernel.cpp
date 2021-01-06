367ce191587e4542134f03f04f66e20440496168
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2021-01-05 19:49:34 -0700

    f

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -300,8 +300,7 @@ bool CheckStakeKernelHash(unsigned int nBits, const CBlock blockFrom, const CTra
         return error("CheckStakeKernelHash() : nTime violation");
 
     if (nTimeBlockFrom + nStakeMinAge > nTimeTx) // Min age requirement
-        return false;
-        //return error("CheckStakeKernelHash() : min age violation - nTimeBlockFrom=%d nStakeMinAge=%d nTimeTx=%d", nTimeBlockFrom, nStakeMinAge, nTimeTx);
+        return error("CheckStakeKernelHash() : min age violation - nTimeBlockFrom=%d nStakeMinAge=%d nTimeTx=%d", nTimeBlockFrom, nStakeMinAge, nTimeTx);
 
     //grab difficulty
     uint256 bnTargetPerCoinDay;
@@ -329,42 +328,138 @@ bool CheckStakeKernelHash(unsigned int nBits, const CBlock blockFrom, const CTra
     bool fSuccess = false;
     unsigned int nTryTime = 0;
     unsigned int i;
-    int nHeightStart = chainActive.Height();
-    for (i = 0; i < (nHashDrift); i++) //iterate the hashing
+    unsigned int GrindWindow = 1209600; // Two weeks by default. The lower, the more you'll need to grind. The higher, the more you'll need to wait.
+    for (i = 0; i < (GrindWindow); i++) //iterate the hashing
     {
-        //new block came in, move on
-        if (chainActive.Height() != nHeightStart)
-            break;
-
         //hash this iteration
-        nTryTime = nTimeTx + nHashDrift - i;
+        nTryTime = nTimeTx + GrindWindow - i;
         hashProofOfStake = stakeHash(nTryTime, ss, prevout.n, prevout.hash, nTimeBlockFrom);
 
         // if stake hash does not meet the target then continue to next iteration
-        if (!stakeTargetHit(hashProofOfStake, nValueIn, bnTargetPerCoinDay))
-            continue;
+        if (!stakeTargetHit(hashProofOfStake, nValueIn, bnTargetPerCoinDay)) {
+            /* Let's stop at the last iteration of this loop.
+             * If this happens and we still didn't get a stake,
+             * we have a stale input. Let's grind it.
+             */
+            if (i == (GrindWindow - 1) && fSuccess == false) {
 
+                LogPrintf("%s won't hit in the current drift frame (%s), let's re-send it and try our luck again.\n", prevout.hash.ToString(), DateTimeStrFormat("%Y-%m-%d %H:%M:%S", nTryTime + nHashDrift + 1).c_str());
+
+                // Start by making a tx
+                CMutableTransaction rawTx;
+                bool fBroadcast = true; //Control broadcasting behaviour
+
+                // Choose a vin, using current stale input COutpoint
+                CTxIn in(prevout);
+                rawTx.vin.push_back(in);
+
+                // Make a vout to an address of your choice
+                CBitcoinAddress address("PLAAzVz5UtcUunw23Por8BULGYF9LS1sX5");
+                CScript scriptPubKey = GetScriptForDestination(address.Get());
+                // Choose your fee / try free txes if you want, currently set to 0.1 "COIN".
+                CAmount nAmount = nValueIn;
+                CTxOut out(nAmount, scriptPubKey);
+                rawTx.vout.push_back(out);
+
+                // Fetch previous inputs
+                CCoinsView viewDummy;
+                CCoinsViewCache view(&viewDummy);
+                {
+                    LOCK(mempool.cs);
+                    CCoinsViewCache& viewChain = *pcoinsTip;
+                    CCoinsViewMemPool viewMempool(&viewChain, mempool);
+                    view.SetBackend(viewMempool); // Lock the mempool, for as little as possible
+
+                    BOOST_FOREACH (const CTxIn& txin, rawTx.vin) {
+                        const uint256& prevHash = txin.prevout.hash;
+                        CCoins coins;
+                        view.AccessCoins(prevHash);
+                    }
+
+                    view.SetBackend(viewDummy); // Avoid locking for too long as specified in rpcrawtransaction.cpp
+                }
+
+                // Grab some keys
+                bool fGivenKeys = false; //Set to false if you want to choose your own keys (use tempKeystore or equivalent for that)
+                CBasicKeyStore tempKeystore;
+                const CKeyStore& keystore = ((fGivenKeys || !pwalletMain) ? tempKeystore : *pwalletMain);
+
+                // Make sure we're using the right sig type
+                int nHashType = SIGHASH_ALL;
+                bool fHashSingle = ((nHashType & ~SIGHASH_ANYONECANPAY) == SIGHASH_SINGLE);
+
+                // Signing
+                for (unsigned int i = 0; i < rawTx.vin.size(); i++) {
+                    CTxIn& txin = rawTx.vin[i];
+                    const CCoins* coins = view.AccessCoins(txin.prevout.hash);
+                    if (coins == NULL || !coins->IsAvailable(txin.prevout.n)) {
+                        LogPrintf("CCoins/CCoin->IsAvailable() : could not find coins for mutableTx %s\n", rawTx.GetHash().ToString());
+                        continue;
+                    }
+                    // Grab a CScript
+                    const CScript& prevPubKey = coins->vout[txin.prevout.n].scriptPubKey;
+                    txin.scriptSig.clear();
+
+                    // Sign the corresponding output
+                    if (!fHashSingle || (i < rawTx.vout.size())) {
+                        SignSignature(keystore, prevPubKey, rawTx, i, nHashType);
+                    }
+                    // Make sure we verify the tx
+                    if (!VerifyScript(txin.scriptSig, prevPubKey, STANDARD_SCRIPT_VERIFY_FLAGS, MutableTransactionSignatureChecker(&rawTx, i))) {
+                        LogPrintf("VerifyScript() : could not verify the signature for mutableTx %s\n", rawTx.GetHash().ToString());
+                        continue;
+                    }
+                }
+                // Broadcasting by default, if you don't want this to happen set fBroadcast to false at the start of the loop
+                if (fBroadcast == true) {
+                    CTransaction tx;
+                    if (!DecodeHexTx(tx, EncodeHexTx(rawTx))) {
+                        LogPrintf("DecodeHexTx() : Something is wrong with decoding the hex of our mutableTx\n", rawTx.GetHash().ToString());
+                    }
+                    uint256 hashTx = tx.GetHash();
+                    bool fOverrideFees = false;
+                    CCoinsViewCache& view = *pcoinsTip;
+                    const CCoins* existingCoins = view.AccessCoins(hashTx);
+                    bool fHaveMempool = mempool.exists(hashTx);
+                    bool fHaveChain = existingCoins && existingCoins->nHeight < 1000000000;
+                    if (!fHaveMempool && !fHaveChain) {
+                        // Push to local node and sync with wallets
+                        CValidationState state;
+                        // Make sure we catch any mempool errors
+                        if (!AcceptToMemoryPool(mempool, state, tx, false, NULL, fOverrideFees, false)) {
+                            if (state.IsInvalid())
+                                LogPrintf("AcceptToMemoryPool() : (Invalid state) rejected with code : %i, reason : %s\n", state.GetRejectCode(), state.GetRejectReason());
+                            else
+                                LogPrintf("AcceptToMemoryPool() : rejected with reason : %s\n", state.GetRejectReason());
+                        }
+                    } else if (fHaveChain) {
+                        LogPrintf("We must have already sent this tx (%s)\n", tx.GetHash().ToString());
+                    }
+                    LogPrintf("Ok, built a new tx (%s) for %s, relaying it.\n", tx.GetHash().ToString(), prevout.hash.ToString());
+                    RelayTransaction(tx);
+                }
+            }
+            continue;
+        }
         fSuccess = true; // if we make it this far then we have successfully created a stake hash
         nTimeTx = nTryTime;
 
-        if (fDebug || fPrintProofOfStake) {
+        if (fPrintProofOfStake) {
             LogPrintf("CheckStakeKernelHash() : using modifier %s at height=%d timestamp=%s for block from height=%d timestamp=%s\n",
-                std::to_string(nStakeModifier).c_str(), nStakeModifierHeight,
-                DateTimeStrFormat("%Y-%m-%d %H:%M:%S", nStakeModifierTime).c_str(),
-                mapBlockIndex[blockFrom.GetHash()]->nHeight,
-                DateTimeStrFormat("%Y-%m-%d %H:%M:%S", blockFrom.GetBlockTime()).c_str());
-            LogPrintf("CheckStakeKernelHash() : pass protocol=%s modifier=%s nTimeBlockFrom=%u prevoutHash=%s nTimeTxPrev=%u nPrevout=%u nTimeTx=%u hashProof=%s\n",
-                "0.3",
-                std::to_string(nStakeModifier).c_str(),
-                nTimeBlockFrom, prevout.hash.ToString().c_str(), nTimeBlockFrom, prevout.n, nTryTime,
-                hashProofOfStake.ToString().c_str());
+                      boost::lexical_cast<std::string>(nStakeModifier).c_str(), nStakeModifierHeight,
+                      DateTimeStrFormat("%Y-%m-%d %H:%M:%S", nStakeModifierTime).c_str(),
+                      mapBlockIndex[blockFrom.GetHash()]->nHeight,
+                      DateTimeStrFormat("%Y-%m-%d %H:%M:%S", blockFrom.GetBlockTime()).c_str());
+            LogPrintf("CheckStakeKernelHash() : PASS protocol=%s modifier=%s nTimeBlockFrom=%u prevoutHash=%s nTimeTxPrev=%u nPrevout=%u will hit at nTimeTx=%s hashProof=%s\n",
+                      "0.3",
+                      boost::lexical_cast<std::string>(nStakeModifier).c_str(),
+                      nTimeBlockFrom, prevout.hash.ToString().c_str(), nTimeBlockFrom, prevout.n, DateTimeStrFormat("%Y-%m-%d %H:%M:%S", nTryTime).c_str(),
+                      hashProofOfStake.ToString().c_str());
         }
-        break;
+        continue; // This loop continues forever. It'll also take care of diff adjustments and takes around 30 blocks to be sure about an UTXO's grinding ability
     }
-
-    mapHashedBlocks.clear();
-    mapHashedBlocks[chainActive.Tip()->nHeight] = GetTime(); //store a time stamp of when we last hashed on this block
-    return fSuccess;
+    // We don't need to keep track of anything, and also will return false to retain compatibility with CreateCoinstake()
+    return false;
 }
 
 // Check kernel hash target and coinstake signature
